56a6f045382dffe290e25472bb2e0b8c68a42c0c
DevDuque <devgduque@gmail.com>
2024-08-24 11:54:00 -0300

    Adiciona métodos de redirecionamento para os botões & adiciona as págicas de forma padrão

--- app/src/main/java/com/example/layoutsapp/MainActivity.java
+++ app/src/main/java/com/example/layoutsapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.layoutsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.layoutsapp.Screens.FormScreen;
+import com.example.layoutsapp.Screens.NewsScreen;
+import com.example.layoutsapp.Screens.ForecastScreen;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Botões das telas
+    private Button btnNews;
+    private Button btnForecast;
+    private Button btnForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Inicializando os botões
+        btnNews = findViewById(R.id.btn_news);
+        btnForecast = findViewById(R.id.btn_forecast);
+        btnForm = findViewById(R.id.btn_form);
+
+        // Métodos de redirecionamento dos botões
+        btnNews.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, NewsScreen.class);
+
+            startActivity(intent);
+        });
+
+        btnForecast.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ForecastScreen.class);
+        });
+
+        btnForm.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FormScreen.class);
+        });
+
+
     }
 }
